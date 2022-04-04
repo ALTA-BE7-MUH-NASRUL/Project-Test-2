14e8772498fd2bee7476d17a3019b2c33e89eed7
nasrul0807 <nasrulmuhammad748@gmail.com>
2022-04-04 23:45:18 +0700

    memperbaiki fitur create project

--- repo/project/interface.go
+++ repo/project/interface.go
@@ -7,7 +7,7 @@ import (
 type ProjectRepositoryInterface interface {
 	GetProject(id int) (_entities.Project, int, error)
 	DeleteProject(id int) (_entities.Project, error)
-	CreateProject(Project _entities.Project) (_entities.Project, error)
+	CreateProject(UserId int, Title string) (_entities.Project, error)
 	UpdatedProject(Project _entities.Project) (_entities.Project, error)
 	CompletedProject(Project _entities.Project) (_entities.Project, error)
 }
