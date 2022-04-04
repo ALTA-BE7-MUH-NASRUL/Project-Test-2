a6ad496949215fab48604125b38a3a561d153c2c
nasrul0807 <nasrulmuhammad748@gmail.com>
2022-04-04 21:30:05 +0700

    membuat entities

--- entities/task.go
+++ entities/task.go
@@ -1,1 +1,16 @@
 package entities
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Task struct {
+	gorm.Model
+	UserID    uint      `json:"UserID"`
+	ProjectID uint      `json:"ProjcetID"`
+	List      string    `json:"list" form:"list"`
+	Deadline  time.Time `json:"deadline" form:"deadline"`
+	Status    string    `json:"status" form:"status"`
+}
--- entities/user.go
+++ entities/user.go
@@ -1,1 +1,12 @@
 package entities
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Name     string    `json:"name" form:"name"`
+	Email    string    `json:"email" form:"email"`
+	Password string    `json:"password" form:"password"`
+	Project  []Project `gorm:"foreignKey:UserID;references:ID"`
+	Task     []Task    `gorm:"foreignKey:UserID;references:ID"`
+}
