dc7f918ad8f808688f92d0c43362fcb174965dfa
nasrul0807 <nasrulmuhammad748@gmail.com>
2022-04-04 23:41:02 +0700

    menghapus deadline pada entities

--- entities/project.go
+++ entities/project.go
@@ -1,16 +1,13 @@
 package entities
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 type Project struct {
 	gorm.Model
-	UserID   uint
-	Title    string    `json:"title" form:"title"`
-	Deadline time.Time `json:"deadline" form:"deadline"`
-	Status   string    `json:"status" form:"status"`
-	Task     []Task    `gorm:"foreignKey:ProjectID;references:ID"`
+	UserID uint
+	Title  string `json:"title" form:"title"`
+	Status string `json:"status" form:"status"`
+	Task   []Task `gorm:"foreignKey:ProjectID;references:ID"`
 }
--- entities/task.go
+++ entities/task.go
@@ -1,16 +1,13 @@
 package entities
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 type Task struct {
 	gorm.Model
-	UserID    uint      `json:"UserID"`
-	ProjectID uint      `json:"ProjcetID"`
-	List      string    `json:"list" form:"list"`
-	Deadline  time.Time `json:"deadline" form:"deadline"`
-	Status    string    `json:"status" form:"status"`
+	UserID    uint   `json:"UserID"`
+	ProjectID uint   `json:"ProjcetID"`
+	List      string `json:"list" form:"list"`
+	Status    string `json:"status" form:"status"`
 }
